f18ab1ea22ec2e5cd61349cd0c1e2e99c5335b0e
FRAMGIA\nguyen.manh.duc <nguyen.manh.duc@framgia.com>
2018-12-10 13:38:59 +0700

    imp. notification from firebase

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
-import React, {Component} from 'react'
-import { StyleSheet, Platform, Image, Text, View } from 'react-native'
+import React, { Component } from 'react'
+import { StyleSheet, Platform, Image, Text, View, AsyncStorage, Alert } from 'react-native'
 import { SwitchNavigator } from 'react-navigation'
 import { createStore, applyMiddleware } from 'redux';
 import createSagaMiddleware from 'redux-saga'
 import { Provider } from 'react-redux'
+import firebase from 'react-native-firebase';
 
 import rootSaga from './src/redux/sagas'
 import allReducers from './src/redux/reducers'
@@ -31,11 +32,68 @@ const AppNavigation = SwitchNavigator(
 )
 
 class App extends Component {
+  async componentDidMount() {
+    const notificationOpen: NotificationOpen = await firebase.notifications().getInitialNotification();
+    if (notificationOpen) {
+        const action = notificationOpen.action;
+        const notification: Notification = notificationOpen.notification;
+        var seen = [];
+        alert(JSON.stringify(notification.data, function(key, val) {
+            if (val != null && typeof val == "object") {
+                if (seen.indexOf(val) >= 0) {
+                    return;
+                }
+                seen.push(val);
+            }
+            return val;
+        }));
+    } 
+    const channel = new firebase.notifications.Android.Channel('test-channel', 'Test Channel', firebase.notifications.Android.Importance.Max)
+            .setDescription('My apps test channel');
+// Create the channel
+    firebase.notifications().android.createChannel(channel);
+    this.notificationDisplayedListener = firebase.notifications().onNotificationDisplayed((notification: Notification) => {
+        // Process your notification as required
+        // ANDROID: Remote notifications do not contain the channel ID. You will have to specify this manually if you'd like to re-display the notification.
+    });
+    this.notificationListener = firebase.notifications().onNotification((notification: Notification) => {
+        // Process your notification as required
+        notification
+            .android.setChannelId('test-channel')
+            .android.setSmallIcon('ic_launcher');
+        firebase.notifications()
+            .displayNotification(notification);
+        
+    });
+    this.notificationOpenedListener = firebase.notifications().onNotificationOpened((notificationOpen: NotificationOpen) => {
+        // Get the action triggered by the notification being opened
+        const action = notificationOpen.action;
+        // Get information about the notification that was opened
+        const notification: Notification = notificationOpen.notification;
+        var seen = [];
+        alert(JSON.stringify(notification.data, function(key, val) {
+            if (val != null && typeof val == "object") {
+                if (seen.indexOf(val) >= 0) {
+                    return;
+                }
+                seen.push(val);
+            }
+            return val;
+        }));
+        firebase.notifications().removeDeliveredNotification(notification.notificationId);
+        
+    });
+}
+componentWillUnmount() {
+    this.notificationDisplayedListener();
+    this.notificationListener();
+    this.notificationOpenedListener();
+}
 
   render() {
     return (
       <Provider store={store}>
-        <AppNavigation ref={nav => {this.navigator = nav}} />
+        <AppNavigation ref={nav => { this.navigator = nav }} />
       </Provider>
     )
   }
